b0d31ede1ae0c61fa6dfba4411d2abd07bae0d74
Felipe <felima.oleveiira@gmail.com>
2020-10-26 13:06:07 -0400

    primeiro commit

---
+++ app/src/main/java/com/example/spotifyanaflavia/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.spotifyanaflavia;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView ivMusic;
+    Button btnPlay;
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ivMusic = (ImageView) findViewById(R.id.ivMusic);
+        Glide.with(this).load("https://i.ytimg.com/vi/Z9Di4wMDg4Y/maxresdefault.jpg").into(ivMusic);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.paredao);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.start();
+    }
+}
